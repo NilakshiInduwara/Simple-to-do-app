693039b5f60d1d323dc0805f7827dff54a3059cf
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-08 12:10:54 +0530

    task list part

--- frontend/src/api/task.api.js
+++ frontend/src/api/task.api.js
@@ -6,3 +6,13 @@ export const createTask = async (task) => {
     const res = await axios.post(API_URL, task);
     return res.data;
 }
+
+export const getLatestFiveTasks = async () => {
+    const res = await axios.get(API_URL);
+    return res.data;
+}
+
+export const updateTaskCompleted = async (id) => {
+    const res = await axios.patch(`${API_URL}/${id}/complete`);
+    return res.data;
+}
